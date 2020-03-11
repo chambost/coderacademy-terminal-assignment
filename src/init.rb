0b6c5f42ba8aded8e1896da2f740bd06fdc72677
Adam Chambost <chambost@users.noreply.github.com>
2020-03-11 14:37:14 +0800

    Added a perfromance example

--- src/init.rb
+++ src/init.rb
@@ -2,6 +2,8 @@
 
 welcome_message = "Here's how this works. I'm going to ask you for some silly words, nouns, verbs, adjectives, etc. They'll be written down and used to act out a dramatic scene."
 ready_message = "Are you ready for this?"
+perform_message = "Okay, alright, we've filled out the words for our scene. Are you ready for the performance?"
+lets_go_messsage = "Here we go, let's go."
 
 require 'pp'
 require 'mac/say'
@@ -10,10 +12,29 @@ require 'tty-prompt'
 prompt = TTY::Prompt.new
 
 host = Mac::Say.new(voice: :karen, rate:225)
+female = Mac::Say.new(voice: :karen, rate:225)
+male = Mac::Say.new(voice: :lee, rate:225)
 
 write_and_say = -> msg {
     puts msg
     host.say string: msg
+}
+
+ask = -> msg {
+    host.say string: msg
+    return prompt.ask(msg)
+}
+
+female_say = -> msg {
+    name = "DOCTOR"
+    puts "#{name}: #{msg}"
+    female.say string: msg
+}
+
+male_say = -> msg {
+    name = "PATIENT"
+    puts "#{name}: #{msg}"
+    male.say string: msg
 }
 
 write_and_say[welcome_message]
@@ -24,24 +45,41 @@ until answer
     answer = prompt.yes?(ready_message)
 end
 
-write_and_say["Silly word"] 
-write_and_say["Type of fish"] 
-write_and_say["Adjective"] 
-write_and_say["What you would shout if you saw a ghost"] 
-write_and_say["A number"] 
-write_and_say["Another number"] 
-write_and_say["A noun"] 
-write_and_say["Body part"] 
-write_and_say["Another adjective"] 
-write_and_say["A type of animal"] 
-write_and_say["A curse word you would say in front of a child"] 
-write_and_say["Amount of time"] 
-write_and_say["Verb ending in i n g"] 
-write_and_say["Another body part "] 
-write_and_say["Verb"] 
-write_and_say["Verb ending in i n g"] 
-write_and_say["Adjective"] 
-write_and_say["Type of profession"] 
-write_and_say["Type of relative"] 
-write_and_say["Male name"] 
-write_and_say["Made up song title"] 
+response1 = ask["Silly word"] 
+response2 = ask["Type of fish"] 
+response3 = ask["Adjective"] 
+response4 = ask["What you would shout if you saw a ghost"] 
+response5 = ask["A number"] 
+response6 = ask["Another number"] 
+
+
+write_and_say[perform_message]
+answer = false
+until answer 
+    answer = prompt.yes?("?")
+end
+write_and_say[lets_go_messsage]
+
+# response7 = ask["A noun"] 
+# response8 = ask["Body part"] 
+# response9 = ask["Another adjective"] 
+# response10 = ask["A type of animal"] 
+# response11 = ask["A curse word you would say in front of a child"] 
+# response12 = ask["Amount of time"] 
+# response13 = ask["Verb ending in i n g"] 
+# response14 = ask["Another body part "] 
+# response15 = ask["Verb"] 
+# response16 = ask["Verb ending in i n g"] 
+# response17 = ask["Adjective"] 
+# response18 = ask["Type of profession"] 
+# response19 = ask["Type of relative"] 
+# response20 = ask["Male name"] 
+# response21 = ask["Made up song title"] 
+
+female_say["Well if it isn't my favourite patient Mr. #{response1}"]
+male_say["Thanks for seeing me at such short notice Dr. #{response2}. So what's the news?"]
+female_say["Well we just finished processing your tests and unfortunately the results are #{response3}"]
+male_say["#{response4}"]
+female_say["I completely understand that reaction. Your blood pressure is #{response5} over #{response6}"]
+
+
