caa99139a455af9aa8e332484b3a44022eff8118
Adam Chambost <chambost@users.noreply.github.com>
2020-03-12 09:06:04 +0800

    DRY applied to asking for response; array from map

--- src/init.rb
+++ src/init.rb
@@ -45,27 +45,51 @@ until answer
     answer = prompt.yes?(ready_message)
 end
 
-response1 = ask["Silly word"] 
-response2 = ask["Type of fish"] 
-response3 = ask["Adjective"] 
-response4 = ask["What you would shout if you saw a ghost"] 
-response5 = ask["A number"] 
-response6 = ask["Another number"] 
-response7 = ask["A noun"] 
-response8 = ask["Body part"] 
-response9 = ask["Another adjective"] 
-response10 = ask["A type of animal"] 
-response11 = ask["A curse word you would say in front of a child"] 
-response12 = ask["Amount of time"] 
-response13 = ask["Verb ending in i n g"] 
-response14 = ask["Another body part"] 
-response15 = ask["Verb"] 
-response16 = ask["Verb ending in i n g"] 
-response17 = ask["Adjective"] 
-response18 = ask["Type of profession"] 
-response19 = ask["Type of relative"] 
-response20 = ask["Male name"] 
-response21 = ask["Made up song title"]
+blanks = [ "Silly word" ,
+    "Type of fish" ,
+    "Adjective" , 
+    "What you would shout if you saw a ghost" ,
+    "A number" ,
+    "Another number" ,
+    "A noun" ,
+    "Body part" ,
+    "Another adjective" ,
+    "A type of animal" ,
+    "A curse word you would say in front of a child" ,
+    "Amount of time" ,
+    "Verb ending in i n g" ,
+    "Another body part" ,
+    "Verb" ,
+    "Verb ending in i n g" ,
+    "Adjective" ,
+    "Type of profession" , 
+    "Type of relative" ,
+    "Male name" ,
+    "Made up song title" ]
+
+responses = blanks.map do |q| ask[q] end
+
+# responses0 = ask["Silly word"] 
+# responses1 = ask["Type of fish"] 
+# responses2 = ask["Adjective"] 
+# responses3 = ask["What you would shout if you saw a ghost"] 
+# responses4 = ask["A number"] 
+# responses5 = ask["Another number"] 
+# responses6 = ask["A noun"] 
+# responses7 = ask["Body part"] 
+# responses8 = ask["Another adjective"] 
+# responses9 = ask["A type of animal"] 
+# responses10 = ask["A curse word you would say in front of a child"] 
+# responses11 = ask["Amount of time"] 
+# responses12 = ask["Verb ending in i n g"] 
+# responses13 = ask["Another body part"] 
+# responses14 = ask["Verb"] 
+# responses15 = ask["Verb ending in i n g"] 
+# responses16 = ask["Adjective"] 
+# responses17 = ask["Type of profession"] 
+# responses18 = ask["Type of relative"] 
+# responses19 = ask["Male name"] 
+# responses20 = ask["Made up song title"]
 
 write_and_say[perform_message]
 answer = false
@@ -74,23 +98,23 @@ until answer
 end
 write_and_say[lets_go_messsage] 
 
-female_say["Well if it isn't my favourite patient Mr. #{response1}"]
-male_say["Thanks for seeing me at such short notice Dr. #{response2}. So what's the news?"]
-female_say["Well we just finished processing your tests and unfortunately the results are #{response3}"]
-male_say["#{response4}"]
-female_say["I completely understand that reaction. Your blood pressure is #{response5} over #{response6}"]
-female_say["And our x-rays found a #{response7} in your #{response8}"]
-female_say["It also appears that you have a rare condition known as #{response9} #{response10}"]
-male_say["#{response11}"]
+female_say["Well if it isn't my favourite patient Mr. #{responses[0]}"]
+male_say["Thanks for seeing me at such short notice Dr. #{responses[1]}. So what's the news?"]
+female_say["Well we just finished processing your tests and unfortunately the results are #{responses[2]}"]
+male_say["#{responses[3]}"]
+female_say["I completely understand that reaction. Your blood pressure is #{responses[4]} over #{responses[5]}"]
+female_say["And our x-rays found a #{responses[6]} in your #{responses[7]}"]
+female_say["It also appears that you have a rare condition known as #{responses[8]} #{responses[9]}"]
+male_say["#{responses[10]}"]
 male_say["Is there any way to cure it?"]
-female_say["I'm going to write you a prescription for these pills. I want you to take one every #{response12}"]
-male_say["But I heard those pills cause involuntary #{response13}"]
-female_say["I'm afraid they do. Side effects also include an itchy #{response14} and a decrease in the desire to #{response15}"]
+female_say["I'm going to write you a prescription for these pills. I want you to take one every #{responses[11]}"]
+male_say["But I heard those pills cause involuntary #{responses[12]}"]
+female_say["I'm afraid they do. Side effects also include an itchy #{responses[13]} and a decrease in the desire to #{responses[14]}"]
 male_say["What can I do to stay healthy?"]
-female_say["I would suggest waking up early every morning and doing stretches and then #{response16} in the park"]
-male_say["That's great advice especially for a #{response17} #{response18} such as myself"]
-female_say["You know the best part about being a doctor is that I get to treat patients like you and then go home to my #{response19} #{response20} and then sing them my favourite song"]
+female_say["I would suggest waking up early every morning and doing stretches and then #{responses[15]} in the park"]
+male_say["That's great advice especially for a #{responses[16]} #{responses[17]} such as myself"]
+female_say["You know the best part about being a doctor is that I get to treat patients like you and then go home to my #{responses[18]} #{responses[19]} and then sing them my favourite song"]
 male_say["Which is?"]
-female_say["#{response21}"]
+female_say["#{responses[20]}"]
 
 
