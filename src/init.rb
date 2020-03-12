84be67a8c477b18f1c8995957a9fa4495e192d91
Adam Chambost <chambost@users.noreply.github.com>
2020-03-12 10:10:18 +0800

    DRY applied to dialgoue; now array and iterates with each

--- src/init.rb
+++ src/init.rb
@@ -70,30 +70,54 @@ blanks = [ "Silly word" ,
     "Male name" ,
     "Made up song title" ]
 
-responses = blanks.map do |q| ask[q] end
+responses = blanks.map(&ask)
 
 write_and_say[perform_message]
 until prompt.yes?("?")
 end
 write_and_say[lets_go_messsage] 
 
-female_say["Well if it isn't my favourite patient Mr. #{responses[0]}"]
-male_say["Thanks for seeing me at such short notice Dr. #{responses[1]}. So what's the news?"]
-female_say["Well we just finished processing your tests and unfortunately the results are #{responses[2]}"]
-male_say["#{responses[3]}"]
-female_say["I completely understand that reaction. Your blood pressure is #{responses[4]} over #{responses[5]}"]
-female_say["And our x-rays found a #{responses[6]} in your #{responses[7]}"]
-female_say["It also appears that you have a rare condition known as #{responses[8]} #{responses[9]}"]
-male_say["#{responses[10]}"]
-male_say["Is there any way to cure it?"]
-female_say["I'm going to write you a prescription for these pills. I want you to take one every #{responses[11]}"]
-male_say["But I heard those pills cause involuntary #{responses[12]}"]
-female_say["I'm afraid they do. Side effects also include an itchy #{responses[13]} and a decrease in the desire to #{responses[14]}"]
-male_say["What can I do to stay healthy?"]
-female_say["I would suggest waking up early every morning and doing stretches and then #{responses[15]} in the park"]
-male_say["That's great advice especially for a #{responses[16]} #{responses[17]} such as myself"]
-female_say["You know the best part about being a doctor is that I get to treat patients like you and then go home to my #{responses[18]} #{responses[19]} and then sing them my favourite song"]
-male_say["Which is?"]
-female_say["#{responses[20]}"]
+# female_say["Well if it isn't my favourite patient Mr. #{responses[0]}"]
+# male_say["Thanks for seeing me at such short notice Dr. #{responses[1]}. So what's the news?"]
+# female_say["Well we just finished processing your tests and unfortunately the results are #{responses[2]}"]
+# male_say["#{responses[3]}"]
+# female_say["I completely understand that reaction. Your blood pressure is #{responses[4]} over #{responses[5]}"]
+# female_say["And our x-rays found a #{responses[6]} in your #{responses[7]}"]
+# female_say["It also appears that you have a rare condition known as #{responses[8]} #{responses[9]}"]
+# male_say["#{responses[10]}"]
+# male_say["Is there any way to cure it?"]
+# female_say["I'm going to write you a prescription for these pills. I want you to take one every #{responses[11]}"]
+# male_say["But I heard those pills cause involuntary #{responses[12]}"]
+# female_say["I'm afraid they do. Side effects also include an itchy #{responses[13]} and a decrease in the desire to #{responses[14]}"]
+# male_say["What can I do to stay healthy?"]
+# female_say["I would suggest waking up early every morning and doing stretches and then #{responses[15]} in the park"]
+# male_say["That's great advice especially for a #{responses[16]} #{responses[17]} such as myself"]
+# female_say["You know the best part about being a doctor is that I get to treat patients like you and then go home to my #{responses[18]} #{responses[19]} and then sing them my favourite song"]
+# male_say["Which is?"]
+# female_say["#{responses[20]}"]
 
 
+dialogue = [ "DOCTOR: Well if it isn't my favourite patient Mr. #{responses[0]}" ,
+"PATIENT: Thanks for seeing me at such short notice Dr. #{responses[1]}. So what's the news?" ,
+"DOCTOR: Well we just finished processing your tests and unfortunately the results are #{responses[2]}" ,
+"PATIENT: #{responses[3]}" ,
+"DOCTOR: I completely understand that reaction. Your blood pressure is #{responses[4]} over #{responses[5]}" ,
+"DOCTOR: And our x-rays found a #{responses[6]} in your #{responses[7]}" ,
+"DOCTOR: It also appears that you have a rare condition known as #{responses[8]} #{responses[9]}" ,
+"PATIENT: #{responses[10]}" ,
+"PATIENT: Is there any way to cure it?" ,
+"DOCTOR: I'm going to write you a prescription for these pills. I want you to take one every #{responses[11]}" ,
+"PATIENT: But I heard those pills cause involuntary #{responses[12]}" ,
+"DOCTOR: I'm afraid they do. Side effects also include an itchy #{responses[13]} and a decrease in the desire to #{responses[14]}" ,
+"PATIENT: What can I do to stay healthy?" ,
+"DOCTOR: I would suggest waking up early every morning and doing stretches and then #{responses[15]} in the park" ,
+"PATIENT: That's great advice especially for a #{responses[16]} #{responses[17]} such as myself" ,
+"DOCTOR: You know the best part about being a doctor is that I get to treat patients like you and then go home to my #{responses[18]} #{responses[19]} and then sing them my favourite song" ,
+"PATIENT: Which is?" ,
+"DOCTOR: #{responses[20]}" ]
+
+actor_to_voice = { 'DOCTOR' => female_say , 'PATIENT' => male_say }
+dialogue.each do |d| 
+    actor, words = d.split(":") 
+    actor_to_voice[actor].call(words)
+end
