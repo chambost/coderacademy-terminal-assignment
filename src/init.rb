5a309f42885ab94e1ea65ccac4fee28399a7de44
Adam Chambost <chambost@users.noreply.github.com>
2020-03-12 19:37:02 +0800

    Refactored ClozeCLI to the classes directory; this will aide testing

--- src/init.rb
+++ src/init.rb
@@ -1,26 +1,5 @@
+#!/usr/bin/env ruby
 
-require 'tty-prompt'
-require 'thor'
-require_relative "modules/cloze_app"
-
-class ClozeCLI < Thor
-    desc "random", "Experience a random Cloze"
-    def random
-        ClozeApp::experience Dir.glob("*.cloze").sample
-    end
-
-    desc "choose", "Choose a Cloze to experience"
-    option :file, :desc => "Specify a .cloze file to experience"
-    def choose 
-        if options[:file]
-            ClozeApp::experience options[:file] 
-        else 
-            clozes = Dir.glob "*.cloze" 
-            cloze = TTY::Prompt.new.select "Which Cloze do you want to experience?" , clozes + ["Random"] 
-            cloze = clozes.sample if cloze == "Random"
-            ClozeApp::experience cloze
-        end
-    end
-end
+require_relative "classes/cloze_cli"
 
 ClozeCLI.start(ARGV)
