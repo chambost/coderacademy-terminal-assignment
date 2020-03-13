87aaa493036832c193ed6c5f3d1b95761d2c255a
Adam Chambost <chambost@users.noreply.github.com>
2020-03-13 13:21:34 +0800

    typo fix - install instead of Install

--- src/install.sh
+++ src/install.sh
@@ -2,6 +2,6 @@
 echo "Installing Improv Cloze Drama app, including bundled gems"
 git clone https://github.com/chambost/coderacademy-terminal-assignment.git
 cd coderacademy-terminal-assignment/src
-bundle Install
+bundle install
 echo "Installation complete. To run type ./run.sh or"
 echo "ruby init.rb choose"
